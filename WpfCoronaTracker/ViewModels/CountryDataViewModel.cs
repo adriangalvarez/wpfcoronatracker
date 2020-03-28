440358d33478ba6d3991f7ba174d05407d21f1d8
Adrian Alvarez <adriangalvarez@gmail.com>
2020-03-28 20:43:26 -0300

    Added Pagination to 20 days per view.

--- WpfCoronaTracker/ViewModels/CountryDataViewModel.cs
+++ WpfCoronaTracker/ViewModels/CountryDataViewModel.cs
@@ -3,20 +3,60 @@ using BECoronaTracker.Models;
 using LiveCharts;
 using LiveCharts.Configurations;
 using LiveCharts.Helpers;
+using System.ComponentModel;
 
 namespace WpfCoronaTracker.ViewModels
 {
-    public class CountryDataViewModel : Conductor<object>
+    public class CountryDataViewModel : Conductor<object>, INotifyPropertyChanged
     {
         public ChartValues<State> Results { get; set; }
         public object Mapper { get; set; }
         public Country Country { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged( string propertyName = null )
+        {
+            if ( PropertyChanged != null )
+                PropertyChanged.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+        }
+
+        private double _from;
+
+        public double From
+        {
+            get{ 
+                return _from; 
+            }
+            set { 
+                _from = value;
+                OnPropertyChanged( "From" );
+            }
+        }
+
+        private double _to;
+
+        public double To
+        {
+            get { 
+                return _to; 
+            }
+            set { 
+                _to = value;
+                OnPropertyChanged( "To" );
+            }
+        }
+
+
+
 
         public CountryDataViewModel( Country country )
         {
             Country = country;
             BECoronaTracker.Controllers.CountryDataController.GetData( Country );
             DrawData();
+            From = 1;
+            To = Country.CurrentDay > 20 ? 20 : Country.CurrentDay;
         }
 
         private void DrawData()
@@ -27,6 +67,24 @@ namespace WpfCoronaTracker.ViewModels
 
             var records = Country.States.ToArray();
             Results = records.AsChartValues();
+        }
+
+        public void Previous()
+        {
+            if ( From > 1 )
+            {
+                From -= 20;
+                To -= 20;
+            }
+        }
+
+        public void Next()
+        {
+            if ( To < Country.CurrentDay )
+            {
+                From += 20;
+                To += 20;
+            }
         }
     }
 }
