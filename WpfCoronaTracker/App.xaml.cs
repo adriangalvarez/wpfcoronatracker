4f2e779c25d1d30f94921b8ce76f83b0cf06a1da
Adrian Alvarez <adriangalvarez@gmail.com>
2020-03-30 12:37:43 -0300

    Removed unused method.

--- WpfCoronaTracker/App.xaml.cs
+++ WpfCoronaTracker/App.xaml.cs
@@ -15,18 +15,6 @@ namespace WpfCoronaTracker
     /// </summary>
     public partial class App : Application
     {
-        private void Application_Startup( object sender, StartupEventArgs e )
-        {
-            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo( "es-AR" );
-
-            ShellView window = new ShellView();
-            if ( e.Args.Length == 1 )
-            {
-                window.Title = $"{e.Args[ 0 ]}";
-            }
-            window.Show();
-        }
-
         private void Application_DispatcherUnhandledException( object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e )
         {
             MessageBox.Show( e.Exception.Message, "Unhandled Exception", MessageBoxButton.OK, MessageBoxImage.Error );
